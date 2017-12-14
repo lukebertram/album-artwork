cc92737bc0878875925ff051ef334ff78fd3c377
Lew Davidson and Luke Bertram <lew+luke@>
2017-12-14 10:10:13 -0800

    add initial scripting for album submit and scroll

--- js/scripts.js
+++ js/scripts.js
@@ -6,14 +6,51 @@ function Album (title, artist, year, art){
   this.art = art;
 }
 
-var albums = [];
+var albumsIndex = 0;
+var defaultAlbum1 = new Album("Glitter", "Mariah Carey", "1922", "img/glitter.jpg");
+var defaultAlbum2 = new Album("Little Criminals", "Randy Newman", "1984", "img/newman.jpg");
+var albums = [defaultAlbum1, defaultAlbum2];
 
+//set current album for display
+var setCurrentAlbum = function(twentyOneJumpStreet){
+  var currentAlbum = albums[twentyOneJumpStreet];
+  $("#currentArt").attr('src', currentAlbum.art );
+  $("#display-title").text(currentAlbum.title);
+  $("#display-artist").text(currentAlbum.artist);
+  $("#display-year").text(currentAlbum.year);
+};
 
 //front end foolery
 $(document).ready(function(){
+  //DEFAULT TO FIRST ALBUM IN ALBUMS
+  setCurrentAlbum(0);
+  //ON PREV CLICK
+  $('#prev').click(function(){
+    if (albums[albumsIndex - 1]) {
+      albumsIndex--;
+      setCurrentAlbum(albumsIndex);
+    }
+  });
+
+  //ON NEXT CLICK
+  $('#next').click(function(){
+    if (albums[albumsIndex + 1]) {
+      albumsIndex++;
+      setCurrentAlbum(albumsIndex);
+    }
+  });
+
+  //ON FORM SUBMIT: add new album from form input
   $('#new-album').submit(function(event){
     event.preventDefault();
-    var title = $('#title')
+    //add validation later
+    var title = $('#title').val();
+    var artist = $('#artist').val();
+    var year = $('#year').val();
+    var art = $('#art').val();
+    var newAlbum = new Album (title, artist, year, art);
+    albums.push(newAlbum);
+
   });
 
 });
